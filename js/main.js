d7a514f9cd08b60ade3bce87e49b49a0df053fc8
manny <immanuelmonroe@example.com>
2025-03-06 14:14:22 -0500

    Adjusted padding for header

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ total = 1000
 
 function betMinimum() {
 
-    reelOne = parseInt(Math.floor(Math.random() * (5 - 1 + 1)) + 1)
-    reelTwo = parseInt(Math.floor(Math.random() * (5 - 1 + 1)) + 1)
-    reelThree = parseInt(Math.floor(Math.random() * (5 - 1 + 1)) + 1)
+    reelOne = parseInt(Math.floor(Math.random() * (5)) + 1)
+    reelTwo = parseInt(Math.floor(Math.random() * (5)) + 1)
+    reelThree = parseInt(Math.floor(Math.random() * (5)) + 1)
 
     document.querySelector('#reelOne').innerHTML = reelOne
     document.querySelector('#reelTwo').innerHTML = reelTwo
@@ -28,9 +28,9 @@ function betMinimum() {
 
 function betMaximum() {
 
-    reelOne = parseInt(Math.floor(Math.random() * (5 - 1 + 1)) + 1)
-    reelTwo = parseInt(Math.floor(Math.random() * (5 - 1 + 1)) + 1)
-    reelThree = parseInt(Math.floor(Math.random() * (5 - 1 + 1)) + 1)
+    reelOne = parseInt(Math.floor(Math.random() * (5)) + 1)
+    reelTwo = parseInt(Math.floor(Math.random() * (5)) + 1)
+    reelThree = parseInt(Math.floor(Math.random() * (5)) + 1)
 
     document.querySelector('#reelOne').innerHTML = reelOne
     document.querySelector('#reelTwo').innerHTML = reelTwo
@@ -45,7 +45,6 @@ function betMaximum() {
     }
 }
 
-//Hello world ! :D
 var coin = document.getElementById("coin");
 function flip() {
     var frameNumber = 1;
